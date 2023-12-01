7cd5b99bb3a2727ab8715a6779f7acee56e940a8
srikanth <20761A0589@lbrce.onmicrosoft.com>
2023-12-01 10:25:33 +0000

    Added NotFound

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import {Switch, Route} from 'react-router-dom'
 import Home from './components/Home'
+import NotFound from './components/NotFound'
 import TeamMatches from './components/TeamMatches'
 import './App.css'
 
@@ -7,6 +8,7 @@ const App = () => (
   <Switch>
     <Route exact path="/" component={Home} />
     <Route exact path="/team-matches/:id" component={TeamMatches} />
+    <Route component={NotFound} />
   </Switch>
 )
 
